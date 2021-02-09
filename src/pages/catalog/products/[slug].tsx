987e551046dee479409c6612abf0bd47e1ce057b
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2021-02-09 15:48:32 -0300

    feat(products): learning components dynamic import

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,10 +1,28 @@
 import { useRouter } from 'next/router'
-// import { Container } from './styles'
+import dynamic from 'next/dynamic'
+import { useCallback, useState } from 'react'
+const AddToCartModal = dynamic(
+  () => import('../../../components/AddToCartModal'),
+  { loading: () => <p>Carregando...</p> , ssr: false}
+)
 
 const products: React.FC = () => {
   const router = useRouter()
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false)
 
-  return <h1>{router.query.slug}</h1>
+  const handleAddToCart = useCallback(() => {
+    setIsAddToCartModalVisible(!isAddToCartModalVisible)
+  }, [isAddToCartModalVisible])
+
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={handleAddToCart}>Add to cart</button>
+
+      {isAddToCartModalVisible && <AddToCartModal />}
+    </div>
+  )
 }
 
 export default products
